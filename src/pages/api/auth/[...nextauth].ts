3570395188e2f2c09deed974cb4a5116cbc1300d
Kilian Henkel <kilian.henkel@outlook.de>
2023-05-17 03:39:05 +0200

    Introduce new Test provider for development purposes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,36 @@
+import logger from '@/server/logger';
 import NextAuth, { AuthOptions } from 'next-auth';
+import { Provider } from 'next-auth/providers';
 import Auth0Provider from 'next-auth/providers/auth0';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+const providers: Provider[] = [Auth0Provider({
+  clientId: process.env.AUTH0_CLIENT_ID ?? '',
+  clientSecret: process.env.AUTH0_CLIENT_SECRET ?? '',
+  issuer: process.env.AUTH0_ISSUER,
+})];
+
+if (process.env.NODE_ENV === 'development') {
+  logger.warning('Development environment: Using local test auth provider');
+  const LocalTestProvider = CredentialsProvider({
+    name: 'Credentials',
+    credentials: {
+      username: { label: "Username", type: "text", placeholder: "jsmith" },
+      password: { label: "Password", type: "password" }
+    },
+    async authorize() {
+      return {
+        id: 'localUserId',
+        email: 'local@test.com',
+        name: 'LocalUser'
+      };
+    }
+  });
+  providers.push(LocalTestProvider);
+}
 
 export const authOptions: AuthOptions = {
-  providers: [
-    Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID ?? '',
-      clientSecret: process.env.AUTH0_CLIENT_SECRET ?? '',
-      issuer: process.env.AUTH0_ISSUER,
-    })
-  ],
+  providers,
   callbacks: {
     session: async ({ session, token }) => {
       return {
